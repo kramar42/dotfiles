13d069c1bd96ce4281ffce053589551f1b718a7f
Oleksii Kramarenko <kramar42@gmail.com>
2023-11-26 01:17:10 +0100

    nvim: gitsigns and mason null-ls

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -85,6 +85,22 @@ vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 -- TODO do i need this?
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 --
+-- icons
+--
+local icons = {
+  diagnostics = {
+    error = " ",
+    warn = " ",
+    hint = " ",
+    info = " ",
+  },
+  git = {
+    added = "",
+    changed = "",
+    deleted = "",
+  },
+}
+--
 -- load lazy
 --
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -126,6 +142,45 @@ require("lazy").setup({
       { "<leader>gf", ":LazyGitFilterCurrentFile<CR>" },
       { "<leader>ge", ":LazyGitFilter<CR>" },
     }
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "BufReadPre",
+    opts = function()
+      --- @type Gitsigns.Config
+      local C = {
+        signs = {
+          add = { text = icons.git.added },
+          change = { text = icons.git.changed },
+          delete = { text = icons.git.deleted },
+          topdelete = { text = icons.git.deleted },
+          changedelete = { text = icons.git.changed },
+          untracked = { text = icons.git.added },
+        },
+        on_attach = function(buffer)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+          end
+
+          map("n", "]g", gs.next_hunk, "Next Hunk")
+          map("n", "[g", gs.prev_hunk, "Prev Hunk")
+          map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+          map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+          map("n", "<leader>gG", gs.stage_buffer, "Stage Buffer")
+          map("n", "<leader>gh", gs.preview_hunk, "Preview Hunk")
+          map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
+          map("n", "<leader>gX", gs.reset_buffer, "Reset Buffer")
+          map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+          map("n", "<leader>gb", function()
+            gs.blame_line({ full = true })
+          end, "Blame Line")
+          map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+        end,
+      }
+      return C
+    end,
   },
   -- telescope
   --
@@ -330,6 +385,32 @@ require("lazy").setup({
       { "rafamadriz/friendly-snippets" },
     },
   },
+
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "jose-elias-alvarez/null-ls.nvim",
+    },
+    config = function()
+      require("mason").setup()
+      require("mason-null-ls").setup({
+        ensure_installed = {
+          -- Opt to list sources here, when available in mason.
+        },
+        automatic_installation = false,
+        handlers = {},
+      })
+      require("null-ls").setup({
+        sources = {
+          -- Anything not supported by mason.
+        }
+      })
+
+    end,
+  },
+
   {
     "Olical/conjure",
     ft = { "clojure" },
