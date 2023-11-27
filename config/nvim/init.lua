6b8b1a497df0631c0198b0a3ca226bb3533092c2
Oleksii Kramarenko <kramar42@gmail.com>
2023-11-27 02:52:53 +0100

    indent

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -424,26 +424,26 @@ require("lazy").setup({
     "Olical/conjure",
     ft = { "clojure" },
     dependencies = {
-        {
-            "PaterJason/cmp-conjure",
-            config = function()
-                local cmp = require("cmp")
-                local config = cmp.get_config()
-                table.insert(config.sources, {
-                    name = "buffer",
-                    option = {
-                        sources = {
-                            { name = "conjure" },
-                        },
-                    },
-                })
-                cmp.setup(config)
-            end,
-        },
+      {
+        "PaterJason/cmp-conjure",
+        config = function()
+          local cmp = require("cmp")
+          local config = cmp.get_config()
+          table.insert(config.sources, {
+            name = "buffer",
+            option = {
+              sources = {
+                { name = "conjure" },
+              },
+            },
+          })
+          cmp.setup(config)
+        end,
+      },
     },
     config = function()
-        require("conjure.main").main()
-        require("conjure.mapping")["on-filetype"]()
+      require("conjure.main").main()
+      require("conjure.mapping")["on-filetype"]()
     end,
   },
   {
